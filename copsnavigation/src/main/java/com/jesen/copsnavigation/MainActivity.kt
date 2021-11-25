8b03eef69c096411877e5be14cfa30587b79f70c
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-11-25 13:31:58 +0800

    [jetpack-banner] 10. add new library for banner demo

--- copsnavigation/src/main/java/com/jesen/copsnavigation/MainActivity.kt
+++ copsnavigation/src/main/java/com/jesen/copsnavigation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jesen.copsnavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
 
     //private val currentRoute = mutableStateOf(Screen.Home.route)
 
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
